983a69410bb2a5bc917842ff17390c526416af0a
Mehman <mixailguliev@mail.ru>
2023-08-08 21:03:05 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,35 @@ import animal.Cat;
 import animal.Dog;
 
 import java.io.*;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.TreeMap;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
 
 public class Main {
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException {
         File file = new File("Animals.txt");
+
         Cat cat = new Cat.CatBuilder().build().generateRandom();
         Dog dog = new Dog.DogBuilder().builder().generateRandom();
         Bird bird = new Bird.BirdBuilder().build().generateRandom();
-        System.out.println(cat);
-        System.out.println(dog);
-        System.out.println(bird);
-        try {
-          PrintWriter writer = new PrintWriter(new FileWriter(file,true));
 
-            Map<String,Integer> map = new TreeMap<>();
-           map.put(cat.getName(), cat.getAge());
-           map.put(dog.getName(), dog.getAge());
-           map.put(bird.getName(), bird.getAge());
-           writer.println(map);
-            writer.close();
+//        System.out.println(cat);
+//        System.out.println(dog);
+//        System.out.println(bird);
 
-        }catch (IOException e){
-            e.printStackTrace();
+        PrintWriter writer = new PrintWriter(new FileWriter(file, true)); //Запись в файл
+        Map<String, Integer> map = new TreeMap<>();
+        map.put(cat.getName(), cat.getAge());
+        map.put(dog.getName(), dog.getAge());
+        map.put(bird.getName(), bird.getAge());
+        writer.println(map);
+        writer.close();
+
+        BufferedReader reader = Files.newBufferedReader(Paths.get("Animals.txt")); // чтение из файла
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
         }
-
-
-
     }
-
-
 }
